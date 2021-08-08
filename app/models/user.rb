7a968ce4f9a54708e1b466448279edf865455096
Kevin De La Torre <kevindelatorre@live.com>
2021-08-08 09:27:54 -0700

    Added a digest function to the User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,10 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::ENGINE.cost
+    BCrypt::Password.create( string, cost: cost )
+  end
 end
