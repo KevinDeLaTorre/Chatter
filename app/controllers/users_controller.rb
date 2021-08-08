2f652f608062c3154f40c28c4c65573f6ef6f636
Kevin De La Torre <kevindelatorre@live.com>
2021-08-08 06:47:56 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new( user_params )
     if @user.save
-      #handle a successful save
+      flash[ :success ] = "Welcome to Chatter!"
+      redirect_to @user
     else
       render 'new'
     end
