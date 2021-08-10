4493a8addf97137d28b6c6c4527b1f6b0a8edca0
Kevin De La Torre <kevindelatorre@live.com>
2021-08-09 20:16:32 -0700

    Fix sorting order on users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.order("created_at ASC").paginate(page: params[:page], per_page: 10)
   end
 
   def show
