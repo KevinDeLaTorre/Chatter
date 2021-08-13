ea0a79d66f54579374be8f16cd4db7462f7fe91c
Kevin De La Torre <kevindelatorre@live.com>
2021-08-13 16:43:16 -0700

    Add tests for password reset mailer.

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -14,4 +14,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
 
+  test "password reset" do
+    user             = users(:kevin)
+    user.reset_token = User.new_token
+    mail             = UserMailer.password_reset(user)
+    assert_equal "Password reset",            mail.subject
+    assert_equal [user.email],                mail.to
+    assert_equal ["kevindelatorre@live.com"], mail.from
+    assert_match user.reset_token,            mail.body.encoded
+    assert_match CGI.escape(user.email),      mail.body.encoded
+  end
+
 end
