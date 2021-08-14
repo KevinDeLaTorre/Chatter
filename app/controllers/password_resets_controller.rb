c99ce7990a5f8143eb6ef7632dcf573055ef10c9
Kevin De La Torre <kevindelatorre@live.com>
2021-08-13 17:10:06 -0700

    Add password reset tests

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -28,8 +28,10 @@ class PasswordResetsController < ApplicationController
       @user.errors.add(:password, "can't be empty")
       render 'edit'
     elsif @user.update(user_params)
+      @user.forget
       reset_session
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
