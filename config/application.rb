ee3ffb3bd6ffa40c7b164fb7966a312de32df2f8
Kevin De La Torre <kevindelatorre@live.com>
2021-08-30 15:50:37 -0700

    add follow functionality

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Chatter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+  
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
